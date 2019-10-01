e983e9ba61e7bbcd3ee219a9acd3b83cf88da662
Adalca0028 <adalca@floridauniversitaria.es>
2019-10-01 13:25:46 +0200

    Little_Animals_01

---
+++ Juego_Android/Assets/Scripts/Movimiento.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 3f * Time.deltaTime, gameObject.transform.position.z);
+    }
+    
+   void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Juego_Android/Assets/Scripts/Spawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject[] loros;
+    public Vector3 spawnValues;
+    public float spawnWait;
+    public float spawnMostWait;
+    public float spawnLeastWait;
+    public int startWait;
+    public bool stop;
+
+    int randLoro;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        StartCoroutine(waitSpawner());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+    }
+
+
+    IEnumerator waitSpawner()
+
+    {
+        yield return new WaitForSeconds (startWait);
+        
+        while (!stop)
+        {
+            randLoro = Random.Range (0, 2);
+            Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), 1, Random.Range (-spawnValues.y, spawnValues.y));
+
+            Instantiate (loros[randLoro], spawnPosition + transform.TransformPoint (0, 0, 0), gameObject.transform.rotation);
+
+            yield return new WaitForSeconds (spawnWait);
+        }
+        
+    }
+
+
+}
+
